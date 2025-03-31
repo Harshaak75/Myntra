f05811a7ab8934a4a46dbd5e40fef68a62cbbd14
Harsha A K <harshaak2003@gmail.com>
2025-03-31 22:38:34 +0530

    testing

--- apps/Backend/src/index.ts
+++ apps/Backend/src/index.ts
@@ -28,8 +28,8 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    // origin: "https://myntra-opal.vercel.app",
-    origin:"http://localhost:5173",
+    origin: "https://myntra-opal.vercel.app",
+    // origin:"http://localhost:5173",
     credentials: true, // ✅ Allows sending cookies & authentication headers
   })
 );
